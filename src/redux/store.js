03f15994e61edb4b5b879ba017bab83ed36a4b74
Alina Melnyk <alinkaaa345@gmail.com>
2021-10-10 02:11:07 +0300

    adds redux toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,29 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-// import { itemsReducer, filterReducer } from "./contacts/contacts-reducer";
-import contactsReducer from './contacts/contacts-reducer';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import { contactsReducer } from './contacts/contacts-reducer';
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
+const middleware = [...getDefaultMiddleware(), logger];
+
+const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
-
 export default store;
+
+// Без использования React Toolkit
+// import { createStore, combineReducers } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import { itemsReducer, filterReducer } from "./contacts/contacts-reducer";
+// import contactsReducer from './contacts/contacts-reducer';
+
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+// });
+
+// const store = createStore(rootReducer, composeWithDevTools());
+
+// export default store;
